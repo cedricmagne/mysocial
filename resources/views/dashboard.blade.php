85b6fc602991820fa2c2c1371c888ecaa9ec98d1
Luiz Felipe <lfmachadodasilva@gmail.com>
2016-02-29 19:35:47 -0300

    Creating the dashboard simple view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,51 @@
-<h1> dashboard </h1>
+@extends('layouts.master')
+
+@section('content')
+  <section class="row new-post">
+    <div class="col-md-6 col-md-offset-3">
+      <header>
+        <h3>
+          What do you have to say?
+        </h3>
+      </header>
+      <form action="#">
+        <div class="form-group">
+          <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Your Post">
+          </textarea>
+        </div>
+        <button type="submit" class="btn btn-primary"> Create Post</button>
+      </form>
+    </div>
+  </section>
+  <section class="row posts">
+    <div class="col-md-6 col-md-offset-3">
+      <header>
+        <h3> What other people say....</h3>
+      </header>
+      <article class="post">
+        <p> foca foca </p>
+        <div class="info">
+          Post by Luiz on date
+        </div>
+        <div class="interaction">
+          <a href="#">Like</a>
+          <a href="#">Dislike</a>
+          <a href="#">Edit</a>
+          <a href="#">Delete</a>
+        </div>
+      </article>
+      <article class="post">
+        <p> foca foca </p>
+        <div class="info">
+          Post by Luiz on date
+        </div>
+        <div class="interaction">
+          <a href="#">Like</a>
+          <a href="#">Dislike</a>
+          <a href="#">Edit</a>
+          <a href="#">Delete</a>
+        </div>
+      </article>
+    </div>
+  </section>
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,13 +3,13 @@
 <head>
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
+  <link rel="stylesheet" href="{{ URL::to('src/css/main.css') }}">
   <title>@yield('title')</title>
 </head>
 <body>
 
   @include('includes.header')
-  
+
   <div class="container">
     @yield('content')
   </div>
